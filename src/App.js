1ed15ff315f2b470764b977a0958129ed450760e
unknown <maria.cioara113@yahoo.com>
2022-05-05 14:26:41 +0300

    small fixes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button} from '@mui/material'
 import { PhotoCamera } from '@mui/icons-material';
 
+const cards=[1,2,3,4,5,6,7,8,9]
+
 function App() {
   return (
     <>
@@ -29,7 +31,8 @@ function App() {
         <div>
           <Container maxWidth="md" className='container'>
             <Grid container spacing={4} className="cardGrid">
-              <Grid item>
+              {cards.map(() => (
+                <Grid item>
                 <Card className="card">
                   <CardMedia
                     className = "cardMedia"
@@ -49,66 +52,7 @@ function App() {
                   </CardActions>
                 </Card>
               </Grid>
-              <Grid item>
-                <Card className="card">
-                  <CardMedia
-                    className = "cardMedia"
-                    image = "https://source.unsplash.com/random"
-                    title = "trip title"
-                  />
-                  <CardContent className="cardContent">
-                    <Typography gutterBottom variant="h5">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. Here you will se the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" marginTop="50px" color="primary">View</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card className="card">
-                  <CardMedia
-                    className = "cardMedia"
-                    image = "https://source.unsplash.com/random"
-                    title = "trip title"
-                  />
-                  <CardContent className="cardContent">
-                    <Typography gutterBottom variant="h5">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. Here you will se the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" marginTop="50px" color="primary">View</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card className="card">
-                  <CardMedia
-                    className = "cardMedia"
-                    image = "https://source.unsplash.com/random"
-                    title = "trip title"
-                  />
-                  <CardContent className="cardContent">
-                    <Typography gutterBottom variant="h5">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. Here you will se the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" marginTop="50px" color="primary">View</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+              ))}
             </Grid>
           </Container>
         </div>
