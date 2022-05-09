c1bae7ae8dded72c20086de33716bd1a22af4355
Bianca Belea <bianca.belea@student.upt.ro>
2022-05-09 13:26:12 +0300

    added navbar for the ViewPost page

--- src/component/ViewPost.js
+++ src/component/ViewPost.js
@@ -1,7 +1,26 @@
 import React from 'react';
- 
-function ViewPost (){
-    return <h3>Here you will be able to see the content of the post. Stay tuned!</h3>
+import logo from './../Logo_Cheap_Trip.png';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button} from '@mui/material'
+import './../App.css';
+import { Link} from 'react-router-dom';
+
+function ViewPost() {
+    return (
+        <>
+            <CssBaseline/>
+            <AppBar position="relative" color="transparent">
+                <Toolbar>
+                    <img src={logo} width="60 px" height="60 px"/>
+                    <Button variant="h6" padding="10px">
+                        Trips
+                    </Button>
+                    <Button variant="h6" padding="10px">
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
 }
  
 export default ViewPost;
