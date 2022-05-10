7e69507e4dd769d4caa9e834fe3f93b818488324
Bianca Belea <bianca.belea@student.upt.ro>
2022-05-10 15:32:09 +0300

    added hardcoded elements for the ViewPost page

--- src/component/ViewPost.js
+++ src/component/ViewPost.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './../Logo_Cheap_Trip.png';
-import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button} from '@mui/material'
+import { Typography, AppBar, CssBaseline, Toolbar, Container, Button} from '@mui/material'
+import { Stack, ListItem} from '@mui/material';
 import './../App.css';
 import { Link} from 'react-router-dom';
+import {Image} from "@mui/icons-material";
 
 function ViewPost() {
     return (
@@ -19,6 +21,24 @@ function ViewPost() {
                     </Button>
                 </Toolbar>
             </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h4" align="center" color="textPrimary" padding="20px" gutterBottom>Title of your trip</Typography>
+                        <Typography variant="h6" align="center" color="textSecondary" paragraph>Here everyone can see your experiences and help others to have a great holiday</Typography>
+                    </Container>
+                </div>
+                <div>
+                    <Stack spacing={2} justifyContent="center" alignItems="center">
+                        <ListItem>Nume</ListItem>
+                        <ListItem>Destinatie</ListItem>
+                        <ListItem>Pret</ListItem>
+                        <ListItem>Restaurante</ListItem>
+                        <ListItem>Cazare</ListItem>
+                        <img height={200} src="https://source.unsplash.com/random"/>
+                    </Stack>
+                </div>
+            </main>
         </>
     );
 }
