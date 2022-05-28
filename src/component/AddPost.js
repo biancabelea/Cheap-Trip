7464bf7cebccd87c1f985eaef9b44eb0c174a214
Bianca Belea <bianca.belea@student.upt.ro>
2022-05-29 00:30:58 +0300

    added the form for AddPost feature

--- src/component/AddPost.js
+++ src/component/AddPost.js
@@ -1,7 +1,91 @@
-import React from 'react';
- 
-function AddPost (){
-    return <h3>Here you will be able to add a post.</h3>
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, TextField} from '@mui/material'
+import logo from './../Logo_Cheap_Trip.png';
+
+function AddPost() {
+    const [form, setForm] = useState({
+        title: "",
+        location: "",
+        price: "",
+        days: "",
+        about: "",
+    });
+    const navigate = useNavigate();
+
+
+    function updateForm(value) {
+        return setForm((prev) => {
+            return { ...prev, ...value };
+        });
+    }
+
+    return (
+        <div>
+            <div><CssBaseline />
+                <AppBar position="relative" color="transparent">
+                    <Toolbar>
+                        <img src={logo} width="60 px" height="60 px"/>
+                        <Button variant="h6" padding="10px" onClick={() => navigate('/')}>
+                            Trips
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+
+            <h3>Create New Post</h3>
+            <Container maxWidth="xs" className="form-group">
+                <TextField
+                    type="text"
+                    className="form-control"
+                    label="title"
+                    id="title"
+                    value={form.title}
+                    onChange={(e) => updateForm({ title: e.target.value })}
+                />
+                <TextField
+                    className="form-control"
+                    label="location"
+                    id="location"
+                    value={form.location}
+                    onChange={(e) => updateForm({ location: e.target.value })}
+                />
+                <TextField
+                    className="form-control"
+                    label="price"
+                    id="price"
+                    value={form.price}
+                    onChange={(e) => updateForm({ price: e.target.value })}
+                />
+                <TextField
+                    className="form-control"
+                    label="days"
+                    id="days"
+                    value={form.days}
+                    onChange={(e) => updateForm({ days: e.target.value })}
+                />
+                <TextField
+                    className="form-control"
+                    label="about"
+                    id="about"
+                    value={form.about}
+                    onChange={(e) => updateForm({ about: e.target.value })}
+                />
+                <br></br>
+                <Button
+                    component="label"
+                    variant="outlined" >
+                    Upload Photo
+                    <input
+                        type="file"
+                        hidden
+                    />
+                </Button>
+                <br></br>
+                <Button variant="contained" type="submit" padding="20px" color="primary" size="large">Add Post</Button>
+            </Container>
+        </div>
+    );
 }
- 
+
 export default AddPost;
